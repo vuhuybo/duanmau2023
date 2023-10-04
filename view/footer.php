823653ec56fbbd51d6438d39bd00de8a760dbd05
ngochuc <ngochuc45@gmail.com>
2023-10-04 17:32:04 +0700

    giao diện chi tiết sản phảm

---
+++ view/footer.php
@@ -0,0 +1,50 @@
+<div class="modal">
+        <div class="overlay" onclick=""></div>
+        <div class="modal_content">
+            <div class="close" onclick="undetail_pro()">
+                <i class="fa-solid fa-x"></i>
+            </div>
+            <img src="access/img/iphone-12-pro-max-xanh-duong-new-600x600-200x200-1.jpg" alt="">
+            <div class="detail_pro">
+                <div class="namedetail mb50">Iphone 12 pro max</div>
+                <div class="pricedetail mb50">1000$</div>
+                <div class="dungluong mb50"> Dung lượng: 128GB</div>
+                <div class="description_pro mb50">
+                    <p>Thông số kĩ thuật</p>
+                    <table>
+                        <tr class="tr">
+                            <td>Kích thước màn hình</td>
+                            <td>6.1 inches</td>
+                        </tr>
+                        <tr >
+                            <td>Công nghệ màn hình</td>
+                            <td>Super Retina XDR OLED</td>
+                        </tr>
+                        <tr class="tr">
+                            <td>Camera sau</td>
+                            <td>Camera góc rộng: 12MP, f/1.6 <br>
+                                Camera góc siêu rộng: 12MP, ƒ/2.4</td>
+                        </tr>
+                        <tr>
+                            <td>Pin</td>
+                            <td>3240mAh</td>
+                        </tr>
+                        
+                    </table>
+                </div>
+                <div class="color mb50">
+
+                    <p for="">Màu sắc</p>
+                    <select name="" id="">
+                        <option value="red">Đỏ</option>
+                        <option value="gray">Xám</option>
+                        <option value="gold">Vàng</option>
+                    </select>
+                </div>
+                <div class="add_btn" style="opacity: 1;margin-top: 10px; margin-right: 90px;">Add to cart</div>
+            </div>
+        </div>
+    </div>
+    <script src="js/chuc.js"></script>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fontawesome-free-6.4.2-web/fontawesome-free-6.4.2-web/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,500;0,700;1,300;1,500;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <nav>
+                <li><a href="">Home</a></li>
+                <li><a href="">Shop all</a></li>
+                <li><a href="">Mobile phones</a></li>
+                <li><a href="">Tablets</a></li>
+                <li><a href="">Phụ kiện</a></li>
+            </nav>
+            <div class="login">
+                <a href="">Đăng nhập</a>
+                <div class="cart"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></div>
+            </div>
+        </header>
